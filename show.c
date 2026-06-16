3265846229894f9ef4ea244436a7452e583ee51a
Rusydi Noor Abdurrahman <rusydinoora@gmail.com>
2026-06-16 14:41:04 +0700

    style: penggantian penggunaan tabel statis menjadi dinamis berdasarkan panjang character

--- show.c
+++ show.c
@@ -1,6 +1,13 @@
 #include "show.h"
 #include <stdio.h>
 
+static void print_line(int length) {
+    for (int i = 0; i < length; i++) {
+        putchar('-');
+    }
+    putchar('\n');
+}
+
 void show(dataset* list){
     dataset* temp = list;
     
@@ -9,17 +16,21 @@ void show(dataset* list){
         return;
     }
     
-    printf ("______________________________________________________________________________________________________________________________________________________\n");
-    printf ("                                                                        DATABASE                                                                      \n");
-    printf ("______________________________________________________________________________________________________________________________________________________\n");
+    int table_width = 139;
+    
+    print_line(table_width);
+    printf ("%*s\n", (table_width + 8) / 2, "DATABASE");
+    print_line(table_width);
     printf ("%-5s | %-25s | %-15s | %-5s | %-13s | %-15s | %-20s | %-20s\n", "ID", "NAMA", "KATEGORI", "STOK", "STATUS(T/D/R)", "LOKASI", "PEMILIK", "PIC");
-    printf ("______________________________________________________________________________________________________________________________________________________\n");
+    print_line(table_width);
     
     while (temp != NULL){
         char status_str[20];
-        sprintf(status_str, "%d/%d/%d", temp->status.tersedia, temp->status.dipinjam, temp->status.rusak);
+        snprintf(status_str, sizeof(status_str), "%d/%d/%d", temp->status.tersedia, temp->status.dipinjam, temp->status.rusak);
         printf ("%-5d | %-25s | %-15s | %-5d | %-13s | %-15s | %-20s | %-20s\n", temp->id, temp->nama, temp->kategori, temp->jumlah_stok, status_str, temp->lokasi_penyimpanan, temp->pemilik, temp->pic);        
         temp = temp->next;
     }
+    print_line(table_width);
     return;
 }
+
