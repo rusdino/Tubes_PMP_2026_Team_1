3c6344df2707c98664121db4e1ed61a618c832cb
son <geraldosilalahi72@gmail.com>
2026-06-18 19:33:39 +0700

    Remove unused function declarations from menu.h

--- menu.h
+++ menu.h
@@ -3,23 +3,7 @@
 
 #include "dataset.h"
 
-/**
- * @brief Seeds initial mock data into the database linked list.
- * 
- * @param head Pointer to the head pointer of the linked list.
- */
 void seed_data(dataset** head);
-
-/**
- * @brief Clears the stdin input buffer.
- */
-void clear_buffer();
-
-/**
- * @brief Runs the main menu interactive CLI loop.
- * 
- * @param head Pointer to the head pointer of the linked list.
- */
 void menu_loop(dataset** head);
 
 #endif
